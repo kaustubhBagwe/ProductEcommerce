2753c495052ba1eec47d5a0425333cddc691e873
Kaustubh Bagwe <kaustubh_bagwe@yahoo.com>
2019-03-03 12:03:57 +0530

    3 March 2018

---
+++ Ecommerce/Models/Admin/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.Admin
+{
+    [Table("Mst_Country")]
+    public class Country
+    {
+        [Key]
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsUpdate { get; set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime LMDDate { get; set; }
+    }
+}
